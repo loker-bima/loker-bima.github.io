c818d459ef02a18caa4f511260c8171e48a74f14
mbojostudio <anharaldevaro789@gmail.com>
2025-07-07 14:07:29 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/loker-bima/', // Ganti dengan nama repositori GitHub kamu
+  base: '/', // Ganti dengan nama repositori GitHub kamu
 });
