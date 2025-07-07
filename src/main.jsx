e94ca085d1761c01e941c9a4417e4db4d82afa12
mbojostudio <anharaldevaro789@gmail.com>
2025-07-07 15:06:37 +0800

    Initial deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,17 @@
 // src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-AOS.init();
-AOS.refresh();
+import 'aos/dist/aos.css'
+import AOS from 'aos'
+AOS.init()
+AOS.refresh()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/"> {/* Ganti sesuai nama repo GitHub */}
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path'; // ✅ Tambahkan baris ini
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ganti sesuai nama repo GitHub kamu
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-});
+})
